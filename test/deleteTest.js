419fae26f9dcbeba73cfaf898b818cbdccbd40b6
kurniacf <kcf170202@gmail.com>
2021-12-30 17:44:30 +0700

    crud test

---
+++ test/deleteTest.js
@@ -0,0 +1,32 @@
+const assert = require("assert");
+const NarutoChar = require("../models/narutocharacter");
+
+describe("Delete records", function(){
+
+    let char;
+
+    beforeEach(function(done){
+        char = new NarutoChar({
+            name: "Naruto"
+        });
+        char.save().then(function(){
+            assert(char.isNew === false);
+            done();
+        });
+    });
+
+    // Dekete
+    it("Delete one record from database", function(done){
+        NarutoChar.findOneAndRemove({
+            name: "Naruto"
+        }).then(function(){
+            NarutoChar.findOne({
+                name: "Naruto"
+            }).then(function(result){
+                assert(result === null);
+                done();
+            })
+        })
+    });
+
+});
